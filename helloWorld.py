0b4b60fff6fa627f83f0237997d6f88957a8cd08
Nathan Haleen <haleen@gmail.com>
2022-04-21 11:33:10 -0500

    added a period

--- helloWorld.py
+++ helloWorld.py
@@ -7,7 +7,7 @@ port = Config.port
 
 @app.route('/', methods=['GET'])
 def index():
-    return "Hello to the World, this is your captain speaking. Masks ARE required on this flight"
+    return "Hello to the World, this is your captain speaking. Masks ARE required on this flight."
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=port, debug=False)
