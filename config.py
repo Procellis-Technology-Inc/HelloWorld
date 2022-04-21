c339210eb6187e5919f3d55623c0c1c92219d4bc
Nathan Haleen <haleen@gmail.com>
2022-04-21 10:16:57 -0500

    switched to Flask

---
+++ config.py
@@ -0,0 +1,2 @@
+class Config(object):
+    port = 10243
--- helloWorld.py
+++ helloWorld.py
@@ -1,1 +1,13 @@
-print("Hello to the World, this is your captain speaking. Masks ARE required on this flight")
+from flask import Flask
+from config import Config
+
+app = Flask(__name__)
+port = Config.port
+
+
+@app.route('/', methods=['GET'])
+def index():
+    return "Hello to the World, this is your captain speaking. Masks ARE required on this flight"
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=port, debug=False)
